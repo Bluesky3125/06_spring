b1418a4743e1fd721775a6bf5e3eeaddb8f933d1
Bluesky3125 <charlie4822@gmail.com>
2025-02-27 14:43:19 +0900

    Spring Commit
    Autowire Setter까지
    2025/02/27 end

---
+++ chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/common/MemberDAO.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.common;
+
+import org.springframework.stereotype.Repository;
+// @Component가 bean을 만드는 어노테이션.
+// @Controller, @Service, @Repository, @Configuration 등이 @Component를 내부에 갖고 있다.(@Component 계열)
+@Repository
+public class MemberDAO {
+}
